53fa4a0bca8f59f9b6fe935410e273a5a410962f
tcbalch <tcbalch@gmail.com>
2024-12-06 19:47:50 -0500

    Initial commit for module7_demo

---
+++ app/db_connect.py
@@ -0,0 +1,30 @@
+import pymysql
+import pymysql.cursors
+from flask import g
+
+def get_db():
+    if 'db' not in g or not is_connection_open(g.db):
+        print("Re-establishing closed database connection.")
+        g.db = pymysql.connect(
+            # Database configuration
+            # Configure MySQL
+            host = 'dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user = 's6eap93dwzisg7n4',
+            password = 'tdbm4qvztrgokyun',
+            database = 'elthminazddyfkqj',
+            cursorclass=pymysql.cursors.DictCursor  # Set the default cursor class to DictCursor
+        )
+    return g.db
+
+def is_connection_open(conn):
+    try:
+        conn.ping(reconnect=True)  # PyMySQL's way to check connection health
+        return True
+    except:
+        return False
+
+def close_db(exception=None):
+    db = g.pop('db', None)
+    if db is not None and not db._closed:
+        print("Closing database connection.")
+        db.close()
